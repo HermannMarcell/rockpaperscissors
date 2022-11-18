d39082c8c996da5be981c82605f011724563eeda
hermannmarcell <hermarcist@gmail.com>
2022-11-18 12:29:33 +0100

    created comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 alert("Let's play a game of Rock Paper and Scissors. The game goes on for 5 rounds.")
 game();
 
+//Gets "rock", "paper" or "scissors" randomly
 function getComputerChoice() {
     let choice = Math.random() * 100;
     if (choice <= 33) {
@@ -14,6 +15,7 @@ function getComputerChoice() {
     };  
 }
 
+//logic for one round of a game, hierarchy of choices
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors"
     || playerSelection === "paper" && computerSelection === "rock"
@@ -25,6 +27,9 @@ function playRound(playerSelection, computerSelection) {
     }
     else return("lose")
   };
+
+//game itself, repeats playRound() 5 times, shows and keeps track of
+//the outcomes and alerts the final result
 
   function game() {
     let winCount = 0;
