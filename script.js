2d66b52d23b109ca20e1a62cbad285df222108d5
hermannmarcell <hermarcist@gmail.com>
2022-11-18 11:54:59 +0100

    created logic for a round of play (combinations --> results)

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 function getComputerChoice() {
     let choice = Math.random() * 100;
     if (choice <= 33) {
-        return ("Rock");
+        return ("rock");
     }
     else if (choice <= 66) {
-        return ("Paper")
+        return ("paper")
     }
     else {
-        return("Scissors")
+        return("scissors")
     };
     
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors"
+    || playerSelection === "paper" && computerSelection === "rock"
+    || playerSelection === "scissors" && computerSelection === "paper") {
+        return("win")
+    }
+    else if (playerSelection === computerSelection) {
+        return("tie")
+    }
+    else return("lose")
+  };
+   
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
